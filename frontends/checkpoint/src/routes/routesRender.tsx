312a71288c1c3fad3e6401e39203a593e7312d2e
Edward Wong <edward.wong@gritus.com>
2022-10-10 23:03:22 +0800

    add not found route

--- frontends/checkpoint/src/routes/routesRender.tsx
+++ frontends/checkpoint/src/routes/routesRender.tsx
@@ -1,19 +1,18 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import { NotFoundPage } from "../pages/NotFoundPage";
 export const RouterRender = () => {
     const renderGeneral = () => {
         return;
     };
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='admin'></Route>
-                <Route path='camman'></Route>
-                <Route path='checkpoint'></Route>
-                <Route path='/'>
-                    <Route path='login'></Route>
-                </Route>
-                <Route path='*' element={}></Route>
-            </Routes>
-        </BrowserRouter>
+        <Routes>
+            <Route path='admin'></Route>
+            <Route path='camman'></Route>
+            <Route path='checkpoint'></Route>
+            <Route path='/'>
+                <Route path='login'></Route>
+            </Route>
+            <Route path='*' element={<NotFoundPage />}></Route>
+        </Routes>
     );
 };
